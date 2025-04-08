dd7b49c282730e36ebc96a63c6dd3bc648504456
Waaatanuki <46774791+Waaatanuki@users.noreply.github.com>
2025-04-08 08:19:31 +0000

    Deploying to gh-pages from @ Waaatanuki/ncnn-webassembly-demo@1a10656d0c279462325da761e1a5fb2e4ed2258e 🚀

---
+++ CMakeFiles/nanodet-simd-threads.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nanodet-simd-threads.dir/nanodet.cpp.o"
+  "CMakeFiles/nanodet-simd-threads.dir/nanodet.cpp.o.d"
+  "CMakeFiles/nanodet-simd-threads.dir/nanodetncnn.cpp.o"
+  "CMakeFiles/nanodet-simd-threads.dir/nanodetncnn.cpp.o.d"
+  "nanodet-simd-threads.js"
+  "nanodet-simd-threads.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nanodet-simd-threads.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/nanodet-simd-threads.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for nanodet-simd-threads.
+# This may be replaced when dependencies are built.
